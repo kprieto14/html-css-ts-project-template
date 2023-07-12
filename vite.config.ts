74f74bfdda93713949022ca240f96b3ca9ba2f16
Kristy Prieto <97772707+kprieto14@users.noreply.github.com>
2023-07-11 20:21:16 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
 import checker from 'vite-plugin-checker'
+import { EsLinter, linterPlugin } from 'vite-plugin-linter'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [checker({ typescript: true })],
-})
+export default defineConfig((configEnv) => ({
+  plugins: [
+    reactRefresh(),
+    checker({
+      // TypeScript config
+      typescript: { tsconfigPath: './tsconfig.json' },
+    }),
+    linterPlugin({
+      disableForBuild: true,
+      include: ['./src/**/*.ts', './src/**/*.tsx'],
+      linters: [
+        new EsLinter({
+          configEnv: configEnv,
+          serveOptions: { cache: false, formatter: 'visualstudio' },
+        }),
+      ],
+    }),
+  ],
+}))
